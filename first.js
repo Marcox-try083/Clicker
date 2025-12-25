7cd5a54a37b918aeba1b06a1a3dbc99d91ec3f37
Marcox-try083 <vishalnitjsr2605ee@gmail.com>
2025-12-25 21:45:03 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,24 @@
+
+const body = document.querySelector('body');
+
+body.addEventListener('click',(e)=>{
+
+    // need to create circle element
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+    circle.textContent = "Hii";
+
+    const color = ['Red','blue','orange','grey','pink','aqua','brown','purple'];
+    circle.style.backgroundColor = color[Math.floor(Math.random()*8)];
+
+    circle.style.top = `${e.clientY}px`;
+    circle.style.left = `${e.clientX}px`;
+
+    body.append(circle);
+
+    //to remove after animation, timeout run once while intervall runs infinite
+    setTimeout(() => {
+        circle.remove(); 
+    }, 5000);
+    
+})
