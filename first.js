f1a66b728d56236043fb5837fa2abbd6129050a4
Marcox-try083 <vishalnitjsr2605ee@gmail.com>
2025-12-25 21:54:21 +0530

    Update first.js

--- first.js
+++ first.js
@@ -23,3 +23,25 @@ body.addEventListener('click',(e)=>{
     
 
 })
+body.addEventListener('mouseenter',(e)=>{
+
+    // need to create circle element
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+    circle.textContent = "Hii";
+
+    const color = ['Red','blue','orange','grey','pink','aqua','brown','purple'];
+    circle.style.backgroundColor = color[Math.floor(Math.random()*8)];
+
+    circle.style.top = `${e.clientY-20}px`;
+    circle.style.left = `${e.clientX-20}px`;
+
+    body.append(circle);
+
+    //to remove after animation, timeout run once while intervall runs infinite
+    setTimeout(() => {
+        circle.remove(); 
+    }, 5000);
+    
+})
+
